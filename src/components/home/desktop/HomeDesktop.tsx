6c4055834455e3006d37fd3253a1fe338009b00e
João Gabriel <viveo.portais@gmail.com>
2025-05-12 12:08:57 -0300

    alteração no layout

--- src/components/home/desktop/HomeDesktop.tsx
+++ src/components/home/desktop/HomeDesktop.tsx
@@ -4,35 +4,36 @@ import { ReactNode } from "react";
 import { Footer } from "@/components/Footer";
 
 interface Props {
-  children: ReactNode;
+ children: ReactNode;
 }
 
 export default function HomeDesktop({ children }: Props) {
-  return (
-    <main className="min-h-screen w-screen flex flex-col">
-      <div className="flex flex-1 w-full">
-        <div className="w-2/3 relative hidden md:flex">
-          <Image
-            src="/images/lilly_background.jpg"
-            alt="Background institucional"
-            fill
-            className="object-cover rounded-tr-[60px] rounded-br-[60px]"
-            priority
-          />
-          <div className="absolute inset-0 bg-white/40" />
-        </div>
+ return (
+  <main className="min-h-screen w-screen flex flex-col">
+   <div className="flex flex-1 w-full">
+    <div className="w-2/3 relative hidden md:flex">
+     <Image
+      src="/images/home.png"
+      alt="Background institucional"
+      fill
+      className="object-cover rounded-tr-[60px] rounded-br-[60px]"
+      priority
+      quality={100}
+     />
+     <div className="absolute inset-0" />
+    </div>
 
-        <div className="w-full md:w-1/3 flex items-center justify-center p-12">
-          <div className="w-full max-w-md md:p-8 md:bg-transparent md:shadow-none">
-            <div className="flex justify-center mb-6">
-              <Image src="/images/logo_lilly.png" alt="Logo Lilly" width={320} height={320} className="mb-10" />
-            </div>
-            {children}
-          </div>
-        </div>
+    <div className="w-full md:w-1/3 flex items-center justify-center p-12">
+     <div className="w-full max-w-md md:p-8 md:bg-transparent md:shadow-none">
+      <div className="flex justify-center mb-6">
+       <Image src="/images/logo_lilly.png" alt="Logo Lilly" width={320} height={320} className="mb-10" />
       </div>
+      {children}
+     </div>
+    </div>
+   </div>
 
-      <Footer bgColor="bg-white" />
-    </main>
-  );
+   <Footer bgColor="bg-white" />
+  </main>
+ );
 }
