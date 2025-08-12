b7d0f95bf277b7de8c029f1626c4e3a7838a4095
Juan Pedro Bispo <p.juanpedro.bispo@viveo.com.br>
2025-08-12 15:33:17 -0300

    Ajuste Embed BI

--- src/app/dashboard/client/report/page.tsx
+++ src/app/dashboard/client/report/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import PowerBIDashboard from "@/components/dashboard/PowerBIDashboard";
+import dynamic from 'next/dynamic';
 
+const PowerBIDashboard = dynamic(
+  () => import("@/components/dashboard/PowerBIDashboard"),
+  { ssr: false }
+);
 
-export default function FilesPage() {
- return <PowerBIDashboard reportId="51edacc5-6ee8-48d7-a98f-ac9aeb9f0f4f" groupId="8a751c4a-5e1e-4158-ba0c-78a5f3280050"/>;
+export default function ReportPage() {
+  return <PowerBIDashboard reportId="51edacc5-6ee8-48d7-a98f-ac9aeb9f0f4f" groupId="8a751c4a-5e1e-4158-ba0c-78a5f3280050"/>;
 }
