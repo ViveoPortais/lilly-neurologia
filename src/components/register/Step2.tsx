d2d3933f1839d707b515e88ba7643fa5f22f2b04
Juan Pedro Bispo <p.juanpedro.bispo@viveo.com.br>
2025-09-25 16:31:28 -0300

    Ajuste Copy Prod

--- src/components/register/Step2.tsx
+++ src/components/register/Step2.tsx
@@ -3,97 +3,101 @@ import { Input } from "@/components/ui/input";
 import { Controller } from "react-hook-form";
 
 interface Step2Props {
- register: any;
- control: any;
- errors: any;
- cellphoneError: string | null;
- setCellphoneError: (value: string | null) => void;
- isDoctorInfoLoading: boolean;
- role: string;
- maskedField: (
-  type: "cpf" | "cellphone",
-  onChange: (e: any) => void,
-  name: string,
-  label: string,
-  disabled: boolean,
-  onBlur: () => void,
-  value: string
- ) => JSX.Element;
- isValidPhoneNumber: (value: string) => boolean;
- setValue: any;
- clearErrors: any;
+  register: any;
+  control: any;
+  errors: any;
+  cellphoneError: string | null;
+  setCellphoneError: (value: string | null) => void;
+  isDoctorInfoLoading: boolean;
+  role: string;
+  maskedField: (
+    type: "cpf" | "cellphone",
+    onChange: (e: any) => void,
+    name: string,
+    label: string,
+    disabled: boolean,
+    onBlur: () => void,
+    value: string
+  ) => JSX.Element;
+  isValidPhoneNumber: (value: string) => boolean;
+  setValue: any;
+  clearErrors: any;
 }
 
 export const Step2 = ({
- register,
- control,
- errors,
- cellphoneError,
- setCellphoneError,
- maskedField,
- isValidPhoneNumber,
- isDoctorInfoLoading,
- role,
- setValue,
- clearErrors,
+  register,
+  control,
+  errors,
+  cellphoneError,
+  setCellphoneError,
+  maskedField,
+  isValidPhoneNumber,
+  isDoctorInfoLoading,
+  role,
+  setValue,
+  clearErrors,
 }: Step2Props) => {
- return (
-  <>
-   <div>
-    <Input
-     {...register("doctorName")}
-     placeholder="Nome Completo"
-     isLoading={role == "medico" ? isDoctorInfoLoading : null}
-     onChange={(e) => {
-      const onlyLetters = e.target.value.replace(/[0-9]/g, "");
-      setValue("doctorName", onlyLetters);
-      clearErrors("doctorName");
-     }}
-     disabled={role == "medico"}
-     required
-    />
-    {errors.doctorName && <span className="text-sm text-red-500">{errors.doctorName.message}</span>}
-   </div>
+  return (
+    <>
+      <div>
+        <Controller
+          name="doctorName"
+          control={control}
+          rules={{ required: true }}
+          defaultValue=""
+          render={({ field }) => (
+            <Input
+              {...field}
+              placeholder="Nome Completo"
+              isLoading={role == "medico" ? isDoctorInfoLoading : undefined}
+              onChange={(e) => {
+                const onlyLetters = e.target.value.replace(/[0-9]/g, "");
+                field.onChange(onlyLetters);
+                clearErrors("doctorName");
+              }}
+              disabled={role === "medico"}
+              required
+            />
+          )}
+        />
+        {errors.doctorName && <span className="text-sm text-red-500">{errors.doctorName.message}</span>}
+      </div>
 
-   <div>
-    <Controller
-     name="cpf"
-     control={control}
-     render={({ field }) => maskedField("cpf", field.onChange, field.name, "CPF", true, () => {}, field.value)}
-    />
-    {errors.cpf && <span className="ml-2 w-full text-sm text-red-500 mt-1">{errors.cpf.message}</span>}
-   </div>
+      <div>
+        <Controller name="cpf" control={control} render={({ field }) => maskedField("cpf", field.onChange, field.name, "CPF", true, () => {}, field.value)} />
+        {errors.cpf && <span className="ml-2 w-full text-sm text-red-500 mt-1">{errors.cpf.message}</span>}
+      </div>
 
-   <div>
-    <Input {...register("emailAddress")} placeholder="E-mail" isLoading={role == "medico" ? isDoctorInfoLoading : null} required />
-    {errors.emailAddress && <span className="text-sm text-red-500">{errors.emailAddress.message}</span>}
-   </div>
+      <div>
+        <Input {...register("emailAddress")} placeholder="E-mail" isLoading={role == "medico" ? isDoctorInfoLoading : null} required />
+        {errors.emailAddress && <span className="text-sm text-red-500">{errors.emailAddress.message}</span>}
+      </div>
 
-   <div>
-    <Controller
-     name="telephoneNumber"
-     control={control}
-     render={({ field }) => {
-      const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-       const value = e.target.value;
-       const cleanPhone = value.replace(/\D/g, "");
-       if (isValidPhoneNumber(cleanPhone)) {
-        field.onChange(value);
-        setCellphoneError(null);
-       } else {
-        setCellphoneError("Telefone inválido");
-        field.onChange(value);
-       }
-      };
+      <div>
+        <Controller
+          name="telephoneNumber"
+          control={control}
+          render={({ field }) => {
+            const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+              const value = e.target.value;
+              const cleanPhone = value.replace(/\D/g, "");
+              if (isValidPhoneNumber(cleanPhone)) {
+                field.onChange(value);
+                setCellphoneError(null);
+              } else {
+                setCellphoneError("Telefone inválido");
+                field.onChange(value);
+              }
+            };
 
-      return maskedField("cellphone", handlePhoneChange, field.name, "Celular", true, () => {}, field.value);
-     }}
-    />
-    <div className="flex flex-col ml-2 mt-1 space-y-1">
-     {cellphoneError && <span className="text-sm text-red-500">{cellphoneError}</span>}
-     {errors.telephoneNumber && <span className="text-sm text-red-500">{errors.telephoneNumber.message}</span>}
-    </div>
-   </div>
-  </>
- );
+            return maskedField("cellphone", handlePhoneChange, field.name, "Celular", true, () => {}, field.value);
+          }}
+        />
+        <div className="flex flex-col ml-2 mt-1 space-y-1">
+          {cellphoneError && <span className="text-sm text-red-500">{cellphoneError}</span>}
+          {errors.telephoneNumber && <span className="text-sm text-red-500">{errors.telephoneNumber.message}</span>}
+        </div>
+      </div>
+    </>
+  );
 };
